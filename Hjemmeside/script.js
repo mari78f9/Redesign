f1ba759730b4e5a2c8bab0586204f8b39338f72d
mari78f9 <mariavbrendstrup@hotmail.com>
2019-05-07 13:20:37 +0200

    menu

--- Hjemmeside/script.js
+++ Hjemmeside/script.js
@@ -4,3 +4,26 @@ $(document).ready(function () {
         $(this).toggleClass("is-active");
     });
 });
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menu").textContent = "☰";
+    } else {
+        document.querySelector("#menu").textContent = "X";
+    }
+}
